8319dec9b04c451232a84ceb02e34e1ccfd06f41
Cengiz Ulusoy <culusoy@ask-cs.com>
2014-02-20 14:31:50 +0100

    working in progress on registration

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -6,10 +6,52 @@ define(
 
     controllers.controller ('register',
       [
-        '$scope',
-        function ($scope)
+        '$scope', '$location',
+        function ($scope, $location)
         {
+          $scope.user = {
+            first: 'Cengiz',
+            last: 'Ulusoy',
+            full: function ()
+            {
+              return this.first + ' ' + this.last;
+            },
+            email: '',
+            password: '',
+            phone: '',
+            verification: ''
+          };
 
+          if (!$location.hash())
+          {
+            $location.hash('step-1');
+          }
+
+          $scope.step = resolveHash();
+
+          function resolveHash ()
+          {
+            return Number($location.hash().split('-')[1]);
+          }
+
+          $scope.stepForward = function ()
+          {
+            $scope.step = Number(resolveHash() + 1);
+
+            console.log('-->', $scope.step);
+
+            $location.hash('step-' + $scope.step);
+          };
+
+          $scope.stepBack = function ()
+          {
+            $scope.step = resolveHash() - 1;
+          };
+
+          $scope.isStep = function (step)
+          {
+            return (($scope.step === step));
+          };
         }
       ]
     );
