a494f34f9814e60dc8696b8ebab3d9d49223303c
lvdinten <lvdinten@ask-cs.com>
2015-07-24 12:41:05 +0200

    new build

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/dist'))
 
-app.get('/', function(request, response) {
+app.get('/', function(req, res) {
     res.redirect("/index.html")
 })
 
