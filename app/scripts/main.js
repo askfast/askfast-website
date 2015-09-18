eee7bd88f6e54fd38b17f5d9b975c104e84792db
lvdinten <lvdinten@ask-cs.com>
2015-09-18 16:46:19 +0200

    header and footer styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -202,7 +202,7 @@ angular.module('AskFast', ['ngRoute'])
   }])
   .controller('PricingController', ['$http', '$rootScope', function($http, $rootScope){
 
-    $http.get('http://sandbox.ask-fast.com/ddr/prices')
+    $http.get('http://api.ask-fast.com/ddr/prices')
     .success(function(data, status, headers, config){
       $rootScope.pricingData = processPricingData(data);
       $rootScope.pricing = $rootScope.pricingData['Netherlands'];
