d8df44cb012b2797b4252f45796eb5746a559dae
Shravan Shetty <sshetty@ask-cs.com>
2015-12-10 12:21:04 +0100

    fix: fixed issue with sending email via contact form

--- app/mail/sendMail.php
+++ app/mail/sendMail.php
@@ -5,21 +5,25 @@ $contactEmail = isset($_REQUEST['contactEmail'])?$_REQUEST['contactEmail']:"";
 $contactSubject = isset($_REQUEST['contactSubject'])?$_REQUEST['contactSubject']:"";
 $contactMessage = isset($_REQUEST['message'])?$_REQUEST['message']:"";
 */
-$inputJSON = file_get_contents('php://input');
-$form= json_decode( $inputJSON, TRUE );
-
 $contactEmail = "noreply <noreply@ask-fast.com>";
 $toEmail = "info@ask-fast.com";
 $isDebug = true;
 function sendMail() {
-        global $toEmail;
+
+	$inputJSON = @file_get_contents('php://input');
+	$form = json_decode( $inputJSON, TRUE );
+	$message = $form["message"];
+	global $toEmail;
         $header =
-                'From: webform@ask-fast.com'. "\r\n";
-                'Reply-To: info@ask-fast.com' . "\r\n";
+                'From: webform@ask-fast.com' . "\r\n" .
+                'Reply-To: ' . $form["emailAddress"] . "\r\n";
+	error_log("header data.. " . $header, 0);
         //risky custom data
         $query = explode('&', $_SERVER['QUERY_STRING'] );
-        $message = $inputJSON;
-        if( $message == '' )return "FALSE";
+        if( $message == '' ) {
+		error_log("No message found for email, exiting..", 0);
+		return "FALSE";
+	}
         global $isDebug;
         if( $isDebug)
         {
@@ -29,8 +33,9 @@ function sendMail() {
                 //mail("skroon@ask-cs.com",  'sendMail form', $message, $header );
                 //mail("hvrooijen@ask-cs.com",  'sendMail form', $message, $header );
         }
-        $ret = mail($toEmail,'contact-formulier',$message ); //,$header);
-        $handle = fopen( 'mail.log', 'ab');
+        //$ret = mail($toEmail,'contact-formulier',$message ); //,$header);
+        $ret = mail($toEmail, 'Contact request from: ' . $form["emailAddress"], $message, $header );
+	$handle = fopen( 'mail.log', 'ab');
         fwrite( $handle, $message."\n".$header );
         fwrite( "\n\n" );
         fclose($handle);
